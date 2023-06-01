2989d98c04069e524b89d9f334f05698085c8b15
Aravinda-N <135206761+Aravinda-N@users.noreply.github.com>
2023-06-01 12:48:45 +0530

    Create Test102.sql

---
+++ Test102.sql
@@ -0,0 +1,18 @@
+ 
+select 
+	ProdUGID,
+	count(resellerid)
+from tblresellers
+where icompany = 11274
+group by ProdUGID 
+having count(resellerid) > 1
+
+select * from tblresellers
+where ProdUGID = 22293545
+and icompany = 11274
+
+select * from tblusers
+where user_id = 'DB3AD846D8154AA187B9E004556E5675'
+
+select * from tblresellers
+where resellerid = 23
